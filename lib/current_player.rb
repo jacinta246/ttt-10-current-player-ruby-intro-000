b5a1c6da014914b668ff4ae9968dab00dd1c6706
Jacinta Calixte <jacinta246@yahoo.com>
2020-03-14 02:09:04 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+#The job of determining how many turns have been played
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def turn_count(board)
+  counter = 0
+  board.each do |element|
+    if element == "X" || element == "O"
+         counter +=1
+    end
+  end
+  counter
+end
+
+#the job of reporting whose turn it currently is, "X" or "O"
+#def current_player
